e4ac150650857bd20ae43451f46b692030d7266c
helfi92 <helfi92@gmail.com>
2016-07-28 21:51:31 -0700

    add component button

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -5,19 +5,37 @@ import {
 	TextInput,
 	StyleSheet
 } from 'react-native';
+import Button from '../common/button';
 
 module.exports = React.createClass({
-	render: function() {
+	getInitialState: function () {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+    render: function() {
 		return (
 			<View style={styles.container}>
 				<Text>Sign In</Text>
 				<Text style={styles.label}>Username:</Text>
-				<TextInput style={styles.input} />
+				<TextInput
+                    style={styles.input}
+                    onChangeText={(username) => this.setState({ username })}
+                    value={this.state.username} />
 				<Text style={styles.label}>Password:</Text>
-				<TextInput secureTextEntry={true} style={styles.input} />
+				<TextInput
+                    secureTextEntry={true}
+                    style={styles.input}
+                    onChangeText={(password) => this.setState({ password })}
+                    value={this.state.password} />
+                <Button text={'Sign In'} onPress={this.onPress} />
 			</View>
 		);
-	}
+	},
+    onPress: function () {
+        // Log user in
+    }
 });
 
 var styles = StyleSheet.create({
