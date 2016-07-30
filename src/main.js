2dfe317b7730946ed5bfae097a46023123a33ecc
helfi92 <helfi92@gmail.com>
2016-07-30 14:51:32 -0700

    add signup component

--- src/main.js
+++ src/main.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import {
-	View,
-	Text,
-	StyleSheet
+	StyleSheet,
+	Navigator
 } from 'react-native';
 
 import Signin from './components/authentication/signin';
 
+var ROUTES = {
+	signin: Signin
+};
+
 module.exports = React.createClass({
-	render: function() {
+	renderScene: function (route, navigator) {
+        var Component = ROUTES[route.name];
+        return <Component />;
+    },
+    
+    render: function() {
 		return (
-			<View style={styles.container}>
-				<Signin />
-			</View>
+			<Navigator
+                style={styles.container}
+                initialRoute={{ name: 'signin' }}
+                renderScene={this.renderScene}
+                configureScene={() => { return Navigator.SceneConfigs.FloatFromRight; }} />
 		);
 	}
 });
 
 var styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
+		flex: 1
 	}
 });
